0c0a2b957c2390621f2328a10c6c122a88502cea
Kris <kris_s2@outlook.com>
2024-01-04 13:50:47 -0800

    Add project files.

---
+++ ConApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.Json;
+using static System.Console;
+
+WriteLine("Welcome to JSON using STO");
+
+//string fileName = "WeatherForecast.json";
+
+WeatherForecast weatherForecast = 
+    new(DateTime.Parse("2023-11-27"), 2, "Cold");
+
+var jsonString = JsonSerializer.Serialize<WeatherForecast>(weatherForecast);
+
+WriteLine(jsonString);
+
+public record WeatherForecast(DateTimeOffset Date, int TemperatureCelsius, string? Summary);
+
