3d63dd7e2512b560b9cff0f45ef4c14a72937195
Kris <kris_s2@outlook.com>
2024-01-14 22:38:48 -0800

    Adding Monkey model and based Monkey data. This will become a base for a local storeg when practicing storage types.

---
+++ ConApp/Model/Monkey.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ConApp.Model;
+public record Monkey
+{
+    public string? Name { get; init; }
+    public string? Location { get; init; }
+    public string? Details { get; init; }
+    public string? Image { get; init; }
+    public int? Population { get; init; }
+    public float? Latitude { get; init; }
+    public float? Longitude { get; init; }
+}
