797a595d8c665996e5426443c428ff54d00e8da5
Kris <kris_s2@outlook.com>
2024-01-17 21:57:55 -0800

    Adding another version of the weatherForecast model. This is used when presenting different JsonIgnore condititions. Presented default/global options, and also JsonIgnore settings for individual props. Added explaines on each used ignore case.

--- ConApp/JsonWriter.cs
+++ ConApp/JsonWriter.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ConApp;
@@ -10,7 +11,7 @@ public static class JsonWriter
     public static async Task WriteJsonToStream(string myFileName)
     {
         WeatherForecast weatherForecast = new(DateTime.Parse("2023-11-27"), 2, "Very Cold");
-        
+
         using FileStream createStream = File.Create(myFileName);
 
         await JsonSerializer.SerializeAsync(createStream, weatherForecast);
@@ -18,13 +19,49 @@ public static class JsonWriter
         await createStream.DisposeAsync();
     }
 
-    public static bool WriteToJsonFile(string myFileName) 
+    public static bool WriteToJsonFile(string myFileName)
     {
         bool result = false;
 
         WeatherForecast weatherForecast = new(DateTime.Parse("2023-11-27"), 2, "Very Cold");
 
         string jsonString = JsonSerializer.Serialize(weatherForecast);
+
+        if (!string.IsNullOrEmpty(jsonString))
+        {
+            File.WriteAllText(@$"Data/{myFileName}", jsonString);
+            result = true;
+        }
+
+        return result;
+    }
+
+    /// <summary>
+    /// This is a 4th episode of James Montagno's series on System.Text.Json. 
+    /// Ignoring Properties when reading & writing JSON.
+    /// </summary>
+    /// <param name="myFileName">A file to serialize to</param>
+    /// <returns>Success or Failure of Serialization</returns>
+    public static bool WriteToJsonFile_OmitProperties(string myFileName)
+    {
+        bool result = false;
+
+        WeatherForecast_v2 weatherForecast = new()
+        {
+            Date = DateTime.Parse("2023-11-27"),
+            Summary = null,
+            TemperatureCelsius = default,
+            WindSpeed = 10
+        };
+
+        JsonSerializerOptions options = new() 
+        {
+            WriteIndented = true,
+            IgnoreReadOnlyProperties = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault & JsonIgnoreCondition.WhenWritingNull
+        }; 
+
+        string jsonString = JsonSerializer.Serialize(weatherForecast, options);
 
         if (!string.IsNullOrEmpty(jsonString))
         {
--- ConApp/Model/WeatherForecast_v2.cs
+++ ConApp/Model/WeatherForecast_v2.cs
@@ -4,9 +4,29 @@ using System.Text.Json.Serialization;
 namespace ConApp.Model;
 public class WeatherForecast_v2
 {
+    [JsonIgnore(Condition =JsonIgnoreCondition.Never)]
     public DateTimeOffset Date { get; set; }
     public int TemperatureCelsius { get; set; }
-    public string Summary { get; set; } = string.Empty;
+
+    [JsonIgnore]
+    public string? Summary { get; set; } = string.Empty;
+    
     [JsonPropertyName("wind")]
     public int WindSpeed { get; set; }
+    public int WindSpeedReadOnly { get; private set; } = 35;
 }
+
+/*
+ * To ignore a property you can add an attribute to it. See the Summary prop. Attribute will take of reading and writing.
+ * 
+ * You can also ignore globally all readonly properties. In this case, use JsonSerializerOptions.
+ * 
+ * When you want to ignore default and/or NULL then model is serialized you can use DefaultIgnoreCondition (this is enum)
+ * 
+ * Another way to add Condition is to add Annotation to the property. See the Date prop.
+ * 
+ * When using JsonSerlizerOptions this is a default setup. When you use attributes for individual properties, they will override the global settings.
+ * 
+ * There is also and Option call JsonIgnoreCondition.Always. This is throw exception if used in the JsonSerializerOptions: 'The value cannot be JsonIgnoreCondition.Always'. 
+ * When used for individual properties, that property will be ignored.
+ */
