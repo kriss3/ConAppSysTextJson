da0219647892fa83c0e5fc29df22cd501129e071
Kris <kris_s2@outlook.com>
2024-01-24 23:46:45 -0800

    Adding a new project (conApp) to attempt to fetch JSON content from the web: Monkeys List. And using System.Net.Http.Json => extension to STJ to deserialize to Monkey object

---
+++ ConAppGetDataFromTheWeb/Monkey.cs
@@ -0,0 +1,11 @@
+﻿namespace ConAppGetDataFromTheWeb;
+public class Monkey
+{
+    public string? Name { get; set; }
+    public string? Location { get; set; }
+    public string? Details { get; set; }
+    public int Population { get; set; }
+    public string? Image { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+}
---
+++ ConAppGetDataFromTheWeb/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+using ConAppGetDataFromTheWeb;
+using System.Text.Json;
+
+WriteLine("Get Monkeys from Internet!");
+
+List<Monkey> monkeyList;
+
+monkeyList = await GetMonkeys();
+
+async Task<List<Monkey>> GetMonkeys() 
+{
+
+    HttpClient _client = new();
+    var myJson = await _client.GetStringAsync("https://www.montemagno.com/monkeys.json");
+    monkeyList = JsonSerializer.Deserialize<List<Monkey>>(myJson) ?? [];
+    return monkeyList;
+}
